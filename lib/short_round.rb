1378f28faf7a3a1f6631937c4d08e9570dfbe0c2
Nick Plante <nap@zerosum.org>
2009-06-22 13:11:12 -0400

    because comments are important

--- lib/short_round.rb
+++ lib/short_round.rb
@@ -2,6 +2,7 @@ class ShortRound
   URL_CHARS = ('0'..'9').to_a + %w(b c d f g h j k l m n p q r s t v w x y z) + %w(B C D F G H J K L M N P Q R S T V W X Y Z - _)
   URL_BASE  = URL_CHARS.size
 
+  # Hang on lady, we going for a ride! 
   def self.generate(id)
     local_count = id
     result = ''
