bb779d30d16d485518374eba2fbf51deffc0c569
Nick Plante <nap@zerosum.org>
2009-07-22 18:54:00 -0400

    extract history / periods partial

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -6,4 +6,23 @@ module GamesHelper
   def can_be_aborted(game)
     logged_in? && (game.owner == current_user) && game.setup?
   end
+
+  def period_votes_summary(period)
+    targets = {}
+    period.events.votes.each do |vote|
+      target = vote.target_player
+      source = vote.source_player
+      if targets[target].nil?
+        targets[target] = [source]
+      else
+        targets[target].push(source)
+      end
+    end
+
+    targets
+  end
+
+  def player_profile_link(player)
+    link_to(player.user.login, twitter_profile_url(player.user), :target => '_blank')
+  end
 end
