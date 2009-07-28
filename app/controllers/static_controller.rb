173582bbb4b81a40650b3fe63cfa8aaea615b253
Nick Plante <nap@zerosum.org>
2009-07-28 19:02:55 -0400

    basic design for homepage, link styles

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,5 @@
 class StaticController < ApplicationController
   def index
-    @users = User.all(:order => "created_at DESC", :limit => 16)
+    @users = User.all(:order => "created_at DESC", :limit => 10)
   end
 end
