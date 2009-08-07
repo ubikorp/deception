636c03cd3b3951b108d48b3cc08d2830931c3f21
Nick Plante <nap@zerosum.org>
2009-08-06 20:01:39 -0400

    make sure game messages are checked before periods are updated

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,16 +5,18 @@
 
 # Example:
 
-set :cron_log, "/var/www/apps/deception/shared/log/cron_log.log"
+set :cron_log, "/var/www/apps/werewolf/shared/log/cron_log.log"
 
 # should be the same as the min_period_length in settings.yml
 every 10.minutes do
+  rake "game:messages:receive"
   rake "game:periods:update"
+  rake "game:messages:send"
 end
 
-every 5.minutes do
-  rake "game:messages:send"
+every 3.minutes do
   rake "game:messages:receive"
+  rake "game:messages:send"
 end
 
 # every 4.days do
