75a7a30c8e0d9b04df2983b0751f2df86339ecd2
Nick Plante <nap@zerosum.org>
2009-09-04 10:20:27 -0400

    update to rails 2.3.4

--- vendor/rails/actionpack/lib/action_controller/reloader.rb
+++ vendor/rails/actionpack/lib/action_controller/reloader.rb
@@ -2,7 +2,8 @@ require 'thread'
 
 module ActionController
   class Reloader
-    @@lock = Mutex.new
+    @@default_lock = Mutex.new
+    cattr_accessor :default_lock
 
     class BodyWrapper
       def initialize(body, lock)
@@ -26,28 +27,28 @@ module ActionController
       end
     end
 
-    def initialize(app, lock = @@lock)
-      @app = app
-      @lock = lock
-    end
-
-    def call(env)
-      @lock.lock
-      Dispatcher.reload_application
-      status, headers, body = @app.call(env)
-      # We do not want to call 'cleanup_application' in an ensure block
-      # because the returned Rack response body may lazily generate its data. This
-      # is for example the case if one calls
-      #
-      #   render :text => lambda { ... code here which refers to application models ... }
-      #
-      # in an ActionController.
-      #
-      # Instead, we will want to cleanup the application code after the request is
-      # completely finished. So we wrap the body in a BodyWrapper class so that
-      # when the Rack handler calls #close during the end of the request, we get to
-      # run our cleanup code.
-      [status, headers, BodyWrapper.new(body, @lock)]
+    def self.run(lock = @@default_lock)
+      lock.lock
+      begin
+        Dispatcher.reload_application
+        status, headers, body = yield
+        # We do not want to call 'cleanup_application' in an ensure block
+        # because the returned Rack response body may lazily generate its data. This
+        # is for example the case if one calls
+        #
+        #   render :text => lambda { ... code here which refers to application models ... }
+        #
+        # in an ActionController.
+        #
+        # Instead, we will want to cleanup the application code after the request is
+        # completely finished. So we wrap the body in a BodyWrapper class so that
+        # when the Rack handler calls #close during the end of the request, we get to
+        # run our cleanup code.
+        [status, headers, BodyWrapper.new(body, lock)]
+      rescue Exception
+        lock.unlock
+        raise
+      end
     end
   end
 end
