070eeb75298277fe56b53f38e82e5d2b6f518213
Darcy Laycock <sutto@sutto.net>
2009-10-24 10:07:20 +0800

    First set of real time events

--- app/models/game_observer.rb
+++ app/models/game_observer.rb
@@ -12,16 +12,17 @@ class GameObserver < ActiveRecord::Observer
       game.players.each do |player|
         GameBot.messages.dm(player.user.login, build_message(:game_start, player.type)) if player.user.notify_start?
       end
+      game.real_time.event! :game_started
     when :continue
       if period = game.periods[-2] # previous period
         notice = period_change_message(period)
-
         # don't send messages to dead players here; handled elsewhere
         game.players.alive.each do |player|
           GameBot.messages.dm(player.user.login, notice) if player.user.notify_period_change?
         end
 
         send_death_notice(period.events.kills.first) if period.events.kills.length > 0 # assume single death in a period
+        game.real_time.event! :period_change
       end
     when :finish
       send_death_notice(game.events.kills.last) if game.events.kills.length > 0 # send final kill notice (last round)
@@ -29,6 +30,7 @@ class GameObserver < ActiveRecord::Observer
       game.players.each do |player|
         GameBot.messages.dm(player.user.login, build_message("game_over_#{game.winner_type}")) if player.user.notify_finish?
       end
+      game.real_time.event! :game_ended
     end
   end
 
@@ -55,18 +57,20 @@ class GameObserver < ActiveRecord::Observer
 
     if kills.length > 0
       victim = kills[0].target_player # assuming a singular victim for now
-
       if period.game.day?
         build_message(:period_summary_am, victim.user.login) + ' ' + build_message(:period_change_am)
       else # period.game.night?
         build_message(:period_summary_pm, victim.user.login) + ' ' + build_message("#{victim.type}_lynch") + build_message(:period_change_pm)
       end
+      event_type = period.game.day? ? :attacked : :lynched
+      period.game.real_time.event! event_type, :user_id => victim.user_id, :user => victim.user.login
     else
       if period.game.day?
         build_message(:period_nodeath_am) + ' ' + build_message(:period_change_am)
       else
         build_message(:period_nodeath_pm) + ' ' + build_message(:period_change_pm)
       end
+      period.game.real_time.event! :no_death
     end
   end
 
--- app/models/player_observer.rb
+++ app/models/player_observer.rb
@@ -1,6 +1,7 @@
 class PlayerObserver < ActiveRecord::Observer
   # auto-start game after max players have joined
   def after_create(player)
+    player.game.real_time.event! :player_joined, :user_id => player.user_id, :user => player.user.login
     if player.game.setup? && player.game.startable? && player.game.full?
       logger.info "Auto-Starting Game [#{player.game.id}]"
       player.game.ready
